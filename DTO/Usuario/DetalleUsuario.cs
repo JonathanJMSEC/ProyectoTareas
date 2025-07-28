57c6882bf2b86e62509b5f835330c416ff613762
JonathanMiskinich <jonathan.miskinich.dev@gmail.com>
2025-07-28 14:01:40 -0300

    feat: agregar dto para listar usuarios

---
+++ DTO/Usuario/DetalleUsuario.cs
@@ -0,0 +1,10 @@
+namespace DTO.Usuario
+{
+    public class DetalleUsuario
+    {
+        public string Nombre { get; set; }
+        public string Email { get; set; }
+        public string FechaCreacion { get; set; }
+        public int CantidadTareas { get; set; }
+    }
+}
