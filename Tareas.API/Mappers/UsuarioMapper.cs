0c4f58839fd1ec264ea4ad662fb024eb7099ecd3
JonathanJMSec <jonathan.miskinich.dev@gmail.com>
2025-07-28 23:28:43 -0300

    feat: agregado mapper de usuario

---
+++ Tareas.API/Mappers/UsuarioMapper.cs
@@ -0,0 +1,45 @@
+using DTO.Usuario;
+using Services.Helpers;
+using Tareas.API.Models;
+
+namespace Tareas.API.Mappers
+{
+    public static class UsuarioMapper
+    {
+        public static Usuario ToModel(CrearUsuarioDTO dto)
+        {
+            if (dto == null)
+                return null;
+
+            string contraseñaHasheada = CalculoHash.GenerarHash(dto.Contrasena);
+
+            return new Usuario(dto.Nombre, dto.Email, contraseñaHasheada);
+        }
+
+        public static DetalleUsuarioDTO ToDTODetalle(Usuario usuario)
+        {
+            if (usuario == null)
+                return null;
+
+            return new DetalleUsuarioDTO
+            {
+                Nombre = usuario.Nombre,
+                Email = usuario.Email,
+                FechaCreacion = usuario.FechaRegistro.ToString("yyyy-MM-dd"),
+                CantidadTareas = usuario.Tareas.Count
+            };
+        }
+
+        public static ResumenUsuarioDTO ToDTOResumen(Usuario usuario)
+        {
+            if (usuario == null)
+                return null;
+
+            return new ResumenUsuarioDTO
+            {
+                Nombre = usuario.Nombre,
+                Email = usuario.Email
+            };
+        }
+    }
+}
