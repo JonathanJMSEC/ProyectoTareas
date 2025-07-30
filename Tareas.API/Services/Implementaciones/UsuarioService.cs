005343905413cae0ebe218d9c2e15e8997b96ae6
JonathanJMSec <jonathan.miskinich.dev@gmail.com>
2025-07-30 17:50:11 -0300

    feat: implementar el servicio de usuario

---
+++ Tareas.API/Services/Implementaciones/UsuarioService.cs
@@ -0,0 +1,42 @@
+using Tareas.API.Models;
+using Tareas.API.Services.Interfaces;
+using Tareas.API.DTO.Usuario;
+using Tareas.API.Repository.Interfaces;
+using Tareas.API.Mappers;
+
+namespace Tareas.API.Services.Implementaciones
+{
+    public class UsuarioService : IUsuarioService
+    {
+        private readonly IRepository<Usuario> _context;
+
+        public UsuarioService(IRepository<Usuario> context)
+        {
+            _context = context;
+        }
+
+        public async Task CrearUsuarioAsync(CrearUsuarioDTO usuario)
+        {
+            await _context.AddAsync(UsuarioMapper.ToModel(usuario));
+        }
+
+        public async Task<Usuario> ObtenerUsuarioPorIdAsync(string id)
+        {
+            return await _context.GetByIdAsync(id);
+        }
+
+        public async Task<IEnumerable<Usuario>> ObtenerTodosLosUsuariosAsync()
+        {
+            return await _context.GetAllAsync();
+        }
+        
+        public async Task ActualizarUsuarioAsync(string id, CrearUsuarioDTO usuario)
+        {
+            var existingUsuario = await _context.GetByIdAsync(id);
+
+            if (existingUsuario != null)
+                await _context.UpdateAsync(id, UsuarioMapper.ToModelActualizar(existingUsuario, usuario));
+            
+        }
+    }
+}
