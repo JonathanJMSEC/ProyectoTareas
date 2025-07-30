3447d3f2e3c7c5826a8b44095b3b5ce1d5a83a84
JonathanJMSec <jonathan.miskinich.dev@gmail.com>
2025-07-30 15:42:39 -0300

    feat:  agregar interfaz de tarea service

---
+++ Tareas.API/Services/Intefaces/ITareaService.cs
@@ -0,0 +1,14 @@
+using Tareas.API.DTO.Tarea;
+using Tareas.API.Models;
+
+namespace Tareas.API.Services.Interfaces
+{
+    public interface ITareaService
+    {
+        Task CrearTareaAsync(CrearTareaDTO tarea);
+        Task<Tarea> ObtenerTareaPorIdAsync(string id);
+        Task ActualizarTareaAsync(string id, ActualizarTareaDTO tarea);
+        Task EliminarTareaAsync(string id);
+        Task<IEnumerable<Tarea>> ObtenerTareasPorUsuarioAsync(string usuarioId);
+    }
+}
