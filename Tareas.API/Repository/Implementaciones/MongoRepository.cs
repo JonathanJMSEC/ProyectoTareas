2f89825292f36adc1a09b61c2fb16511de0de772
JonathanJMSec <jonathan.miskinich.dev@gmail.com>
2025-07-29 23:34:26 -0300

    feat: agregar interfaz repositorio y implementacion del repo con mongo

---
+++ Tareas.API/Repository/Implementaciones/MongoRepository.cs
@@ -0,0 +1,45 @@
+using MongoDB.Bson;
+using MongoDB.Driver;
+using Tareas.API.Repository.Interfaces;
+
+namespace Tareas.API.Repository.Implementaciones
+{
+    public class MongoRepository<T> : IRepository<T> where T : class
+    {
+        private IMongoCollection<T> Collection { get; }
+
+        public MongoRepository(IMongoDatabase database, string collectionName)
+        {
+            Collection = database.GetCollection<T>(collectionName);
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            var filter = Builders<T>.Filter.Empty;
+            return await Collection.Find(filter).ToListAsync();
+        }
+
+        public Task<T> GetByIdAsync(string id)
+        {
+            var filter = Builders<T>.Filter.Eq("_id", ObjectId.Parse(id));
+            return Collection.Find(filter).FirstOrDefaultAsync();
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await Collection.InsertOneAsync(entity);
+        }
+
+        public Task UpdateAsync(string id, T entity)
+        {
+            var filter = Builders<T>.Filter.Eq("_id", ObjectId.Parse(id));
+            return Collection.ReplaceOneAsync(filter, entity);
+        }
+
+        public Task DeleteAsync(string id)
+        {
+            var filter = Builders<T>.Filter.Eq("_id", ObjectId.Parse(id));
+            return Collection.DeleteOneAsync(filter);
+        }
+    }
+}
---
+++ Tareas.API/Repository/Interfaces/IRepository.cs
@@ -0,0 +1,11 @@
+namespace Tareas.API.Repository.Interfaces
+{   
+    public interface IRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetByIdAsync(string id);
+        Task AddAsync(T entity);
+        Task UpdateAsync(string id, T entity);
+        Task DeleteAsync(string id);
+    }
+}
