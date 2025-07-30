522c5fa8af45110198d56ebc6825166df048b741
JonathanJMSec <jonathan.miskinich.dev@gmail.com>
2025-07-30 15:38:32 -0300

    refactor: cambio  nombre  namespace y eliminado atributo  accesibilidad  de los metodos

--- Tareas.API/Services/Intefaces/IUsuarioService.cs
+++ Tareas.API/Services/Intefaces/IUsuarioService.cs
@@ -2,13 +2,13 @@ using Tareas.API.DTO.Usuario;
 
 using Tareas.API.Models;
 
-namespace Tareas.API.Services
+namespace Tareas.API.Services.Interfaces
 {
     public interface IUsuarioService
     {
-        public Task CrearUsuarioAsync(CrearUsuarioDTO usuario);
-        public Task<Usuario> ObtenerUsuarioPorIdAsync(string id);
-        public Task<IEnumerable<Usuario>> ObtenerTodosLosUsuariosAsync();
-        public Task<IEnumerable<Tarea>> ObtenerTareasUsuarioAsync(string id);
+        Task CrearUsuarioAsync(CrearUsuarioDTO usuario);
+        Task<Usuario> ObtenerUsuarioPorIdAsync(string id);
+        Task<IEnumerable<Usuario>> ObtenerTodosLosUsuariosAsync();
+        Task<IEnumerable<Tarea>> ObtenerTareasUsuarioAsync(string id);
     }
 }
