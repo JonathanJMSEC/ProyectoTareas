99c454d3e362672c9bbef4b5f060b62c06a39c81
JonathanJMSec <jonathan.miskinich.dev@gmail.com>
2025-07-31 16:57:32 -0300

    feat: cambiar firma de los metodos

--- Tareas.API/Services/Intefaces/ITareaService.cs
+++ Tareas.API/Services/Intefaces/ITareaService.cs
@@ -1,13 +1,14 @@
 using Tareas.API.DTO.Tarea;
 using Tareas.API.Models;
+using Tareas.API.Services.Implementaciones;
 
 namespace Tareas.API.Services.Interfaces
 {
     public interface ITareaService
     {
-        Task CrearTareaAsync(CrearTareaDTO tarea);
-        Task<Tarea> ObtenerTareaPorIdAsync(string id);
-        Task ActualizarTareaAsync(string id, ActualizarTareaDTO tarea);
-        Task EliminarTareaAsync(string id);
+        Task<ServiceResponse<Tarea>> CrearTareaAsync(CrearTareaDTO tarea);
+        Task<ServiceResponse<Tarea>> ObtenerTareaPorIdAsync(string id);
+        Task<ServiceResponse<Tarea>> ActualizarTareaAsync(string id, ActualizarTareaDTO tarea);
+        Task<ServiceResponse<Tarea>> EliminarTareaAsync(string id);
     }
 }
