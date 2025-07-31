3e86925eee6b45eeff211e4961a2e0f5968400ed
JonathanJMSec <jonathan.miskinich.dev@gmail.com>
2025-07-31 16:21:36 -0300

    feat: agregar un service response

---
+++ Tareas.API/Services/Implementaciones/ServiceResponse.cs
@@ -0,0 +1,26 @@
+namespace Tareas.API.Services.Implementaciones
+{
+    public class ServiceResponse<T>
+    {
+        public T Data { get; set; }
+        public string Message { get; set; }
+        public bool Success { get; set; }
+
+        public ServiceResponse(T data, string message = "", bool success = true)
+        {
+            Data = data;
+            Message = message;
+            Success = success;
+        }
+
+        public static ServiceResponse<T> Ok(T data, string message = "")
+        {
+            return new ServiceResponse<T>(data, message, true);
+        }
+
+        public static ServiceResponse<T> Error(string message)
+        {
+            return new ServiceResponse<T>(default, message, false);
+        }
+    }
+}
