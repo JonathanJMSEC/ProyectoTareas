c9d33f5b8cfe04f2065f46d30c11f5fc4553c6ed
JonathanJMSec <jonathan.miskinich.dev@gmail.com>
2025-07-31 23:20:55 -0300

    fix: cambiar  validaciones por los  metodos

--- Tareas.API/Services/Implementaciones/TareaService.cs
+++ Tareas.API/Services/Implementaciones/TareaService.cs
@@ -3,6 +3,7 @@ using Tareas.API.Services.Interfaces;
 using Tareas.API.DTO.Tarea;
 using Tareas.API.Repository.Interfaces;
 using Tareas.API.Mappers;
+using Tareas.API.Services.Helpers;
 
 namespace Tareas.API.Services.Implementaciones
 {
@@ -17,14 +18,7 @@ namespace Tareas.API.Services.Implementaciones
 
         public async Task<ServiceResponse<Tarea>> CrearTareaAsync(CrearTareaDTO tarea)
         {
-            if (tarea == null)
-                return ServiceResponse<Tarea>.Error("Tarea no puede ser nula");
-
-            if (string.IsNullOrWhiteSpace(tarea.Titulo))
-                return ServiceResponse<Tarea>.Error("El título de la tarea es obligatorio");
-
-            if (tarea.FechaLimite < DateTime.Now)
-                return ServiceResponse<Tarea>.Error("La fecha límite no puede ser anterior a la fecha actual");
+            ValidacionesTarea.EsTareaValida(tarea);
 
             var tareaACrear = TareaMapper.ToModel(tarea);
 
@@ -34,35 +28,24 @@ namespace Tareas.API.Services.Implementaciones
 
         public async Task<ServiceResponse<Tarea>> ObtenerTareaPorIdAsync(string id)
         {
-            if (string.IsNullOrWhiteSpace(id))
-                return ServiceResponse<Tarea>.Error("El ID de la tarea es obligatorio");
+            ValidacionesTarea.EsIdValido(id);
             
             var tarea = await _context.GetByIdAsync(id);
 
-            if (tarea == null)
-                return ServiceResponse<Tarea>.Error("Tarea no encontrada");
+            ValidacionesTarea.EsTareaExistente(id, tarea);
             
             return ServiceResponse<Tarea>.Ok(tarea, "Tarea obtenida exitosamente");
         }
 
         public async Task<ServiceResponse<Tarea>> ActualizarTareaAsync(string id, ActualizarTareaDTO tarea)
         {
-            if (string.IsNullOrWhiteSpace(id))
-                return ServiceResponse<Tarea>.Error("El ID de la tarea es obligatorio");
+            ValidacionesTarea.EsIdValido(id);
                 
-            if (tarea == null)
-                return ServiceResponse<Tarea>.Error("Tarea no puede ser nula");
-
-            if (string.IsNullOrWhiteSpace(tarea.Titulo) || string.IsNullOrWhiteSpace(tarea.Descripcion))
-                return ServiceResponse<Tarea>.Error("El título y el estado de la tarea es obligatorio");
-
-            if (tarea.FechaLimite < DateTime.Now)
-                return ServiceResponse<Tarea>.Error("La fecha límite no puede ser anterior a la fecha actual");
+            ValidacionesTarea.EsTareaActualizable(id, tarea);
 
             var existingTarea = await _context.GetByIdAsync(id);
 
-            if (existingTarea == null)
-                return ServiceResponse<Tarea>.Error("Tarea no encontrada");
+            ValidacionesTarea.EsTareaExistente(id, existingTarea);
 
             await _context.UpdateAsync(id, TareaMapper.ToModel(existingTarea, tarea));
             
@@ -71,13 +54,11 @@ namespace Tareas.API.Services.Implementaciones
 
         public async Task<ServiceResponse<Tarea>> EliminarTareaAsync(string id)
         {
-            if (string.IsNullOrWhiteSpace(id))
-                return ServiceResponse<Tarea>.Error("El ID de la tarea es obligatorio");
+            ValidacionesTarea.EsIdValido(id);
 
             var tarea = await _context.GetByIdAsync(id);
 
-            if (tarea == null)
-                return ServiceResponse<Tarea>.Error("Tarea no encontrada");
+            ValidacionesTarea.EsTareaExistente(id, tarea);
 
             await _context.DeleteAsync(id);
             
