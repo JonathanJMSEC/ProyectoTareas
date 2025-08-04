10cd57c8afc52a3f6c46a453f7b366dee10a2301
JonathanJMSec <jonathan.miskinich.dev@gmail.com>
2025-08-04 14:11:43 -0300

    fix: cambiar  firma para que coincida con interfaz

--- Tareas.API/Services/Implementaciones/TareaService.cs
+++ Tareas.API/Services/Implementaciones/TareaService.cs
@@ -26,15 +26,15 @@ namespace Tareas.API.Services.Implementaciones
         /// </summary>
         /// <param name="tarea">Los datos de la tarea a crear</param>
         /// <returns>Un resultado indicando si se completó la tarea y la tarea</returns>
-        public async Task<ServiceResponse<Tarea>> CrearTareaAsync(CrearTareaDTO tarea)
+        public async Task<ServiceResponse<ResumenTareaDTO>> CrearTareaAsync(CrearTareaDTO tarea)
         {
             if (!ValidacionesTarea.EsTareaValida(tarea).Success)
-                return ServiceResponse<Tarea>.Error("Los datos de la tarea son inválidos.");
+                return ServiceResponse<ResumenTareaDTO>.Error("Los datos de la tarea son inválidos.");
 
             var tareaACrear = TareaMapper.ToModel(tarea);
 
             await _context.AddAsync(tareaACrear);
-            return ServiceResponse<Tarea>.Ok(tareaACrear, "Tarea creada exitosamente");
+            return ServiceResponse<ResumenTareaDTO>.Ok(TareaMapper.ToResumenDTO(tareaACrear), "Tarea creada exitosamente");
         }
 
         /// <summary>
@@ -43,17 +43,17 @@ namespace Tareas.API.Services.Implementaciones
         /// </summary>
         /// <param name="id">ID de la tarea a obtener</param>
         /// <returns>Respuesta con la tarea obtenida y mensaje de éxito o error.</returns
-        public async Task<ServiceResponse<Tarea>> ObtenerTareaPorIdAsync(string id)
+        public async Task<ServiceResponse<ListarTareaDTO>> ObtenerTareaPorIdAsync(string id)
         {
             if (!ValidacionesTarea.EsIdValido(id).Success)
-                return ServiceResponse<Tarea>.Error("El ID de la tarea es inválido.");
+                return ServiceResponse<ListarTareaDTO>.Error("El ID de la tarea es inválido.");
 
             var tarea = await _context.GetByIdAsync(id);
 
             if (!ValidacionesTarea.EsTareaExistente(id, tarea).Success)
-                return ServiceResponse<Tarea>.Error("La tarea no existe.");
+                return ServiceResponse<ListarTareaDTO>.Error("La tarea no existe.");
 
-            return ServiceResponse<Tarea>.Ok(tarea, "Tarea obtenida exitosamente");
+            return ServiceResponse<ListarTareaDTO>.Ok(TareaMapper.ToListarDTO(tarea), "Tarea obtenida exitosamente");
         }
 
         /// <summary>
@@ -61,22 +61,22 @@ namespace Tareas.API.Services.Implementaciones
         /// Valida que el ID de la tarea sea correcto y que la tarea exista.
         /// </summary>
         /// <param name="tarea">DTO con los nuevos datos de la tarea y un mensaje  de exito o error</param
-        public async Task<ServiceResponse<Tarea>> ActualizarTareaAsync(string id, ActualizarTareaDTO tarea)
+        public async Task<ServiceResponse<ResumenTareaDTO>> ActualizarTareaAsync(string id, ActualizarTareaDTO tarea)
         {
             if (!ValidacionesTarea.EsIdValido(id).Success)
-                return ServiceResponse<Tarea>.Error("El ID de la tarea es inválido.");
+                return ServiceResponse<ResumenTareaDTO>.Error("El ID de la tarea es inválido.");
 
             if (!ValidacionesTarea.EsTareaActualizable(id, tarea).Success)
-                return ServiceResponse<Tarea>.Error("Los datos de la tarea son inválidos.");
+                return ServiceResponse<ResumenTareaDTO>.Error("Los datos de la tarea son inválidos.");
 
             var existingTarea = await _context.GetByIdAsync(id);
 
             if (!ValidacionesTarea.EsTareaExistente(id, existingTarea).Success)
-                return ServiceResponse<Tarea>.Error("La tarea no existe.");
+                return ServiceResponse<ResumenTareaDTO>.Error("La tarea no existe.");
 
             await _context.UpdateAsync(id, TareaMapper.ToModel(existingTarea, tarea));
 
-            return ServiceResponse<Tarea>.Ok(existingTarea, "Tarea actualizada exitosamente");
+            return ServiceResponse<ResumenTareaDTO>.Ok(TareaMapper.ToResumenDTO(existingTarea), "Tarea actualizada exitosamente");
         }
 
         /// <summary>
@@ -84,19 +84,19 @@ namespace Tareas.API.Services.Implementaciones
         /// Valida que el ID de la tarea sea correcto y que la tarea exista.
         /// </summary>
         /// <returns>Respuesta con la tarea eliminada y mensaje de éxito o error.</returns
-        public async Task<ServiceResponse<Tarea>> EliminarTareaAsync(string id)
+        public async Task<ServiceResponse<ResumenTareaDTO>> EliminarTareaAsync(string id)
         {
             if  (!ValidacionesTarea.EsIdValido(id).Success)
-                return ServiceResponse<Tarea>.Error("El ID de la tarea es inválido.");
+                return ServiceResponse<ResumenTareaDTO>.Error("El ID de la tarea es inválido.");
 
             var tarea = await _context.GetByIdAsync(id);
 
             if (!ValidacionesTarea.EsTareaExistente(id, tarea).Success)
-                return ServiceResponse<Tarea>.Error("La tarea no existe.");
+                return ServiceResponse<ResumenTareaDTO>.Error("La tarea no existe.");
 
             await _context.DeleteAsync(id);
 
-            return ServiceResponse<Tarea>.Ok(tarea, "Tarea eliminada exitosamente");
+            return ServiceResponse<ResumenTareaDTO>.Ok(TareaMapper.ToResumenDTO(tarea), "Tarea eliminada exitosamente");
         }
     }
 }
