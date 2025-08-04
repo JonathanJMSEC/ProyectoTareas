3eeeef0d3bbceffcb31d0b543ac1193a3a7a1576
JonathanJMSec <jonathan.miskinich.dev@gmail.com>
2025-08-04 18:44:09 -0300

    fix: implementar la  interfaz de  tares repositorio

--- Tareas.API/Repository/Implementaciones/TareaRepository.cs
+++ Tareas.API/Repository/Implementaciones/TareaRepository.cs
@@ -6,35 +6,39 @@ using Tareas.API.DTO.Tarea;
 
 namespace Tareas.API.Repository.Implementaciones
 {
-    public class TareaRepository : MongoRepository<Usuario>, IRepository<Usuario>
+    public class TareaRepository : ITareaRepository
     {
+        private readonly MongoRepository<Usuario> _baseRepo;
 
-        public TareaRepository(IMongoDatabase database) : base(database, "Usuarios") { }
+        public TareaRepository(IMongoDatabase database)
+        {
+            _baseRepo = new MongoRepository<Usuario>(database, "Usuarios");
+        }
 
         public async Task<IEnumerable<Tarea>> GetTareasAsync(string idUsuario)
         {
-            Usuario usuario = await base.GetByIdAsync(idUsuario);
+            Usuario usuario = await _baseRepo.GetByIdAsync(idUsuario);
             return usuario?.Tareas ?? Enumerable.Empty<Tarea>();
         }
 
         public async Task<Tarea> GetByIdAsync(string idTarea, string idUsuario)
         {
-            Usuario usuario = await base.GetByIdAsync(idUsuario);
+            Usuario usuario = await _baseRepo.GetByIdAsync(idUsuario);
             return usuario?.Tareas.FirstOrDefault(t => t.Id == ObjectId.Parse(idTarea));
         }
 
         public async Task AddAsync(string idUsuario, Tarea entity)
         {
-            Usuario usuario = await GetByIdAsync(idUsuario);
+            Usuario usuario = await _baseRepo.GetByIdAsync(idUsuario);
             if (usuario == null) throw new Exception("Usuario no encotrado");
 
             usuario.Tareas.Add(entity);
-            await base.UpdateAsync(idUsuario, usuario);
+            await _baseRepo.UpdateAsync(idUsuario, usuario);
         }
 
         public async Task UpdateAsync(string idUsuario, CrearTareaDTO tareaDto, string idTarea)
         {
-            var usuario = await base.GetByIdAsync(idUsuario);
+            var usuario = await _baseRepo.GetByIdAsync(idUsuario);
         if (usuario == null) throw new Exception("Usuario no encontrado");
 
         var tareaExistente = usuario.Tareas.FirstOrDefault(t => t.Id == ObjectId.Parse(idTarea));
@@ -44,19 +48,19 @@ namespace Tareas.API.Repository.Implementaciones
         tareaExistente.Descripcion = tareaDto.Descripcion;
         tareaExistente.FechaLimite = tareaDto.FechaLimite;
 
-        await base.UpdateAsync(idUsuario, usuario);
+        await _baseRepo.UpdateAsync(idUsuario, usuario);
         }
 
         public async Task DeleteAsync(string idUsuario, string idTarea)
         {
-            var usuario = await base.GetByIdAsync(idUsuario);
+            var usuario = await _baseRepo.GetByIdAsync(idUsuario);
             if (usuario == null) throw new Exception("Usuario no encontrado");
 
             var tareaABorrar = usuario.Tareas.FirstOrDefault(t => t.Id == ObjectId.Parse(idTarea));
             if (tareaABorrar == null) throw new Exception("Tarea no encontrada");
 
             usuario.Tareas.Remove(tareaABorrar);
-            await base.UpdateAsync(idUsuario, usuario);
+            await _baseRepo.UpdateAsync(idUsuario, usuario);
         }
     }
 }
